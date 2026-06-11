8a80e1cec3e9385184108594e23369c4f687da3f
Wibill37 <minjunjeon080421@gmail.com>
2026-06-11 09:03:48 +0900

    근접 공격 캐릭터 설계 - dummy.py, combat.py 파일 추가

--- brawlhalla_clone/core/input_state.py
+++ brawlhalla_clone/core/input_state.py
@@ -1,42 +1,47 @@
 # core/input_state.py
 
-
 class InputState:
     """
     한 프레임의 입력 상태.
 
     *_pressed  : 이번 프레임에 처음 눌린 경우만 True
     *_released : 이번 프레임에 떼어진 경우만 True
-    held       : 현재 누르고 있는 상태
     """
 
     __slots__ = (
         "left", "right", "up", "down",
         "jump", "jump_pressed", "jump_released",
         "dodge", "dodge_pressed",
+        "attack", "attack_pressed",
+        "ultimate", "ultimate_pressed",
     )
 
     def __init__(self) -> None:
-        # 현재 누르고 있는지
-        self.left:  bool = False
+        self.left: bool = False
         self.right: bool = False
-        self.up:    bool = False
-        self.down:  bool = False
-        self.jump:  bool = False
-        self.dodge: bool = False
+        self.up: bool = False
+        self.down: bool = False
 
-        # 이번 프레임 이벤트
-        self.jump_pressed:   bool = False
-        self.jump_released:  bool = False
-        self.dodge_pressed:  bool = False
+        self.jump: bool = False
+        self.jump_pressed: bool = False
+        self.jump_released: bool = False
+
+        self.dodge: bool = False
+        self.dodge_pressed: bool = False
+
+        self.attack: bool = False
+        self.attack_pressed: bool = False
+
+        self.ultimate: bool = False
+        self.ultimate_pressed: bool = False
 
     def reset_frame_events(self) -> None:
-        """매 프레임 시작 시 1프레임 이벤트 초기화"""
-        self.jump_pressed  = False
+        self.jump_pressed = False
         self.jump_released = False
         self.dodge_pressed = False
+        self.attack_pressed = False
+        self.ultimate_pressed = False
 
     @property
     def move_x(self) -> int:
-        """수평 입력 방향. -1 / 0 / 1"""
         return int(self.right) - int(self.left)
